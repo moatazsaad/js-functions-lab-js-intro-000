407d939c85316a7cbf3745b09af55e591773330e
moataz osama <moataz.osama@hotmail.com>
2020-03-15 08:44:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ function happyHolidays()
  {return "Happy holidays!";}
 function happyHolidaysTo(moataz)
 {return `Happy holidays, ${moataz}!`}
+function happyCustomHolidayTo( July 3rd,moataz)
+{return `Happy ${July 3rd}, ${moataz}!`}
