ab524458000fac4ff034a0a11d525ec53fc407a7
moataz osama <moataz.osama@hotmail.com>
2020-03-15 08:38:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 //var = happyHolidays
-function happyHolidays()
- {return "Happy holidays!";}
-functionhappyHolidaysTo(moataz)
+// function happyHolidays()
+//  {return "Happy holidays!";}
+function happyHolidaysTo(moataz)
 {return Happy holidays, ${moataz}!}
